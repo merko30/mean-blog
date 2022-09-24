6b2e91a059fe0a74a38a7acc4c5deac6a8317ea0
Merim <merim.hasanbegovic@outlook.com>
2022-09-24 15:30:00 +0200

    update login and button components

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { ButtonVariant } from 'src/constants';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.css']
+  styleUrls: ['./button.component.css'],
 })
 export class ButtonComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  @Input() type: ButtonVariant = ButtonVariant.primary;
+  @Input() class: string = '';
 }
